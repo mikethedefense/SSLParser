9dcf1c6cdd2cd6c26b6520455a0700831133e2c8
FARMER <41405040+mikethedefense@users.noreply.github.com>
2025-08-13 11:39:05 -0400

    Added: - Loops - Recursive functionality (Nesting functions) - Exit When function

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import json
 import sys
 import os
 
-# TODO: build repeat until functionality
-
+# TODO: add if-else capability
+# TODO: add database functionality (look-up tables)
 
 class App: # Master Controller
     def __init__(self,master):
@@ -25,6 +25,114 @@ class App: # Master Controller
     def show_frame(self,page_name):
         frame = self.frames[page_name]
         frame.tkraise()
+
+    # Recursive Helpers
+    def load_rules_from_data(self,parent_container, rules_data, indent=0):
+        frames = []
+        for rule in rules_data:
+            rf = _RulesFrame(parent_container, indent_level=indent)
+            rf.selected_option.set(rule['type'])
+
+            if rule['type'] == 'Set':
+                rf.signal_entry_variable.set(rule['signal'])
+                rf.sig_val_entry_variable.set(rule['value'])
+
+            elif rule['type'] == 'Wait':
+                rf.wait_time.set(rule['wait_time'])
+
+            elif rule['type'] == 'Prompt':
+                rf.prompt.set(rule['prompt'])
+
+            elif rule['type'] in ('If', 'Exit When'):
+                # Load conditions
+                for j, cond_data in enumerate(rule['conditions']):
+                    if j > 0:
+                        rf.add_conditions_row()
+                    cond_row = rf.conditions[j]
+                    cond_row.var_name.set(cond_data['variable'])
+                    cond_row.operator.set(cond_data['operator'])
+                    cond_row.value.set(cond_data['value'])
+                    cond_row.logic_op.set(cond_data['logic_op'])
+                # Load sub-rules recursively
+                if 'sub_rules' in rule:
+                    rf.sub_rules = self.load_rules_from_data(rf.sub_rules_container, rule['sub_rules'], indent + 1)
+
+            elif rule['type'] == 'Loop':
+                rf.sub_rules = self.load_rules_from_data(rf.sub_rules_container, rule['sub_rules'], indent + 1)
+
+            rf.rule_frame.grid(row=len(frames) + 1, column=0, sticky='w')
+            frames.append(rf)
+        return frames
+
+    def export_rule_to_json_and_ssi(self,rule_frame, indent=0):
+        """Returns (json_data, ssi_code) for a single _RulesFrame"""
+        prefix = "\t" * indent
+        rule_type = rule_frame.selected_option.get()
+
+        if rule_type == 'Set':
+            sig = rule_frame.signal_entry_variable.get()
+            val = rule_frame.sig_val_entry_variable.get()
+            json_data = {"type": "Set", "signal": sig, "value": val}
+            code = f'{prefix}{sig} =: {val if val.isdigit() else f'"{val}"'};\n'
+            return json_data, code
+
+        elif rule_type == 'Wait':
+            wt = rule_frame.wait_time.get()
+            return {"type": "Wait", "wait_time": wt}, f"{prefix}wait({wt},True);\n"
+
+        elif rule_type == 'Prompt':
+            p = rule_frame.prompt.get()
+            return {"type": "Prompt", "prompt": p}, f'{prefix}prompt("{p}");\n'
+
+        elif rule_type in ('If', 'Exit When'):
+            # Conditions
+            conds = []
+            cond_strs = []
+            op_map = {'Equals': '=',
+                      'Greater Than': '>',
+                      'Less Than': '<',
+                      'Not Equals': '/='}
+            for cond in rule_frame.conditions:
+                cd = {
+                    "variable": cond.var_name.get(),
+                    "operator": cond.operator.get(),
+                    "value": cond.value.get(),
+                    "logic_op": cond.logic_op.get()
+                }
+                conds.append(cd)
+                op = op_map.get(cd['operator'])
+                if cd['value'].isdigit():
+                    cond_str = f"{cd['variable']} {op} {cd['value']}"
+                else:
+                    cond_str = f'{cd["variable"]} {op} "{cd["value"]}"'
+                if cd['logic_op']:
+                    cond_str += f' {cd["logic_op"].lower()} '
+                cond_strs.append(cond_str)
+
+            # Sub Rules
+            sub_json = []
+            sub_code = ""
+            for sr in rule_frame.sub_rules:
+                sj, sc = self.export_rule_to_json_and_ssi(sr, indent + 1)
+                sub_json.append(sj)
+                sub_code += sc
+
+            json_data = {"type": rule_type, "conditions": conds, "sub_rules": sub_json}
+
+            if rule_type == 'If':
+                return json_data, f"{prefix}if {''.join(cond_strs)} then\n{sub_code}{prefix}end if\n"
+            elif rule_type == 'Exit When':
+                return json_data, f"{prefix}exit when {''.join(cond_strs)};\n"
+
+        elif rule_type == 'Loop':
+            sub_json = []
+            sub_code = ""
+            for sr in rule_frame.sub_rules:
+                sj, sc = self.export_rule_to_json_and_ssi(sr, indent + 1)
+                sub_json.append(sj)
+                sub_code += sc
+            return {"type": "Loop", "sub_rules": sub_json}, f"{prefix}loop\n{sub_code}{prefix}end loop\n"
+
 
 class InitPage(Frame):
     def __init__(self,parent, controller): # the parent is the frame
@@ -68,47 +176,10 @@ class InitPage(Frame):
         # Create pages from JSON
         for i, cfg in enumerate(data["configs"]):
             page = TestCreationPage(self.controller.container, self.controller, i, total)
-            for rule in cfg["rules"]:
-                r = _RulesFrame(page)
-                r.selected_option.set(rule["type"])
-                if rule["type"] == "Set":
-                    r.signal_entry_variable.set(rule["signal"])
-                    r.sig_val_entry_variable.set(rule["value"])
-                elif rule["type"] == "Wait":
-                    r.wait_time.set(rule["wait_time"])
-                elif rule["type"] == "Prompt":
-                    r.prompt.set(rule["prompt"])
-                elif rule['type'] == 'If':
+            page.rules_list = self.controller.load_rules_from_data(page, cfg['rules'])
+            for idx, rule_frame in enumerate(page.rules_list):
+                rule_frame.rule_frame.grid(row = idx + 1, column = 0, sticky = 'w')
 
-                    # Populate the conditions
-                    for j,cond_data in enumerate(rule['conditions']):
-                        if j>0:
-                            r.add_conditions_row()
-                        cond_row = r.conditions[j]
-                        cond_row.var_name.set(cond_data['variable'])
-                        cond_row.operator.set(cond_data['operator'])
-                        cond_row.value.set(cond_data['value'])
-                        cond_row.logic_op.set(cond_data['logic_op'])
-                    # Populate sub-rules
-                    for sub in rule['sub_rules']:
-                        sub_rule = _RulesFrame(r.sub_rules_container, ['Set', 'Wait', 'Prompt', 'Repeat Until'])
-                        sub_rule.selected_option.set(sub['type'])
-                        if sub['type'] == 'Set':
-                            sub_rule.signal_entry_variable.set(sub['signal'])
-                            sub_rule.sig_val_entry_variable.set(sub['value'])
-                        elif sub['type'] == 'Wait':
-                            sub_rule.wait_time.set(sub['wait_time'])
-                        elif sub['type'] == 'Prompt':
-                            sub_rule.prompt.set(sub['prompt'])
-                        elif sub['type'] == 'Repeat Until':
-                            # TODO
-                            pass
-                        sub_rule.rule_frame.grid(row = len(r.sub_rules) + 1, column =1, padx = (20,0))
-                        r.sub_rules.append(sub_rule)
-
-                r.rule_frame.grid(row=page.row_counter.get(), column=0, sticky = 'w')
-                page.rules_list.append(r)
-                page.row_counter.set(page.row_counter.get() + 1)
 
             self.controller.frames[f'Config{i}'] = page
             page.grid(row=0, column=0, sticky='nsew')
@@ -194,104 +265,9 @@ class TestCreationPage(Frame):
                 code_string += f'\n-- Config {page.index + 1}\n'
 
                 for rule in page.rules_list:
-                    # If more rules are added, more conditions need to be added.
-                    if rule.selected_option.get() == 'Set':
-                        sig = rule.signal_entry_variable.get()
-                        val = rule.sig_val_entry_variable.get()
-                        config_data["rules"].append({
-                            "type": "Set",
-                            "signal": sig,
-                            "value": val
-                        })
-                        if val.isdigit():
-                            code_string += f'{sig} =: {val};\n'
-                        elif val.isalpha():
-                            code_string += f'{sig} =: "{val}";\n'
-
-                    elif rule.selected_option.get() == 'Wait':
-                        wait_time = rule.wait_time.get()
-                        config_data["rules"].append({
-                            "type": "Wait",
-                            "wait_time": wait_time,
-                        })
-                        code_string += f'wait({wait_time},True );\n'
-                    elif rule.selected_option.get() == 'Prompt':
-                        prompt = rule.prompt.get()
-                        config_data["rules"].append({
-                            "type": "Prompt",
-                            "prompt": prompt,
-                        })
-                        code_string += f'prompt("{prompt}");\n'
-                    elif rule.selected_option.get() == 'If':
-                        conditions_data = []
-                        cond_strings = [] # to be joined for ssl code.
-                        op_map = {   # Mapping gui operators to the SSL
-                            'Equals': '=',
-                            'Greater Than': ">",
-                            'Less Than': "<",
-                            'Not Equals': '<>'
-                        }
-                        for cond in rule.conditions:
-                            cond_data = {
-                                "variable":cond.var_name.get(),
-                                "operator":cond.operator.get(),
-                                "value":cond.value.get(),
-                                "logic_op": cond.logic_op.get()
-                            }
-                            ssl_op = op_map.get(cond_data['operator']) # Lookup the matching operator
-                            if cond_data['value'].isdigit():
-                                cond_str = f"{cond_data['variable']} {ssl_op} {cond_data["value"]}" # Combine the expressions
-                            elif cond_data['value'].isalpha():
-                                cond_str = f'{cond_data['variable']} {ssl_op} "{cond_data['value']}"'
-                            if cond_data['logic_op']:
-                                cond_str += f' {cond_data['logic_op'].lower()} '
-                            conditions_data.append(cond_data)
-                            cond_strings.append(cond_str)
-                        if_rule_data = {
-                            "type": "If",
-                            "conditions": conditions_data,
-                            "sub_rules": []
-                        }
-                        ssl_condition = "".join(cond_strings).strip() # Remove whitespace
-                        code_string += f'if {ssl_condition} then\n'
-
-                        # Handle sub rules
-                        for sub_rule in rule.sub_rules:
-                            if sub_rule.selected_option.get() == 'Set':
-                                sig = sub_rule.signal_entry_variable.get()
-                                val = sub_rule.sig_val_entry_variable.get()
-                                if_rule_data['sub_rules'].append({
-                                    'type': 'Set',
-                                    'signal': sig,
-                                    'value': val
-                                })
-                                if val.isdigit():
-                                    code_string += f'\t {sig} =: {val};\n'
-                                else:
-                                    code_string += f'\t {sig} =: "{val}";\n'
-                            elif sub_rule.selected_option.get() == 'Wait':
-                                wait_time =sub_rule.wait_time.get()
-                                if_rule_data['sub_rules'].append({
-                                    'type': 'Wait',
-                                    'wait_time': wait_time,
-                                })
-                                code_string += f'\t wait({wait_time},True );\n'
-                            elif sub_rule.selected_option.get() == 'Prompt':
-                                prompt = sub_rule.prompt.get()
-                                if_rule_data['sub_rules'].append({
-                                    'type': 'Prompt',
-                                    'prompt': prompt,
-                                })
-                                code_string += f'\t prompt("{prompt}");\n'
-                            elif sub_rule.selected_option.get() == 'Repeat Until':
-                                # TODO
-                                pass
-                        code_string += 'end if \n'
-                        config_data["rules"].append(if_rule_data)
-
-
-
-
+                    json_rule, ssi_code = self.controller.export_rule_to_json_and_ssi(rule)
+                    config_data["rules"].append(json_rule)
+                    code_string += ssi_code
 
                 # Create the config ssi files
                 ssi_config_path = os.path.join("Test Procedures",tp_name, f'Config{page.index}.ssi')
@@ -346,8 +322,11 @@ class _RulesFrame:
     """
     Private class used to define the rules base
     """
-    def __init__(self,controller, options = ['Set','Wait','Prompt','If', 'Repeat Until']):
+    def __init__(self,controller, options = ['Set','Wait','Prompt','If', 'Loop'], indent_level = 0):
         self.controller = controller
+        self.indent_level = indent_level
+        self.sub_rules = []
+        self.conditions = []
 
         # Variables
         self.options = options
@@ -358,7 +337,7 @@ class _RulesFrame:
         # Labels and entries to use (more can be added)
         self.selected_option = StringVar()
         self.selector = OptionMenu(self.rule_frame, self.selected_option, *self.options)
-        self.selector.grid(row=0, column=0)
+        self.selector.grid(row=0, column=0, padx = (self.indent_level * 20,0))
         self.del_btn = Button(self.rule_frame, text='Delete', command=self.delete_rule)
 
         # ----Rules----
@@ -401,26 +380,33 @@ class _RulesFrame:
             # Handling logic containers
             self.if_container = Frame(self.rule_frame)
             self.if_container.grid(row = 0,column = 1)
-            self.conditions = []
             self.add_conditions_row()
             # Handling sub-rule Containers
             self.sub_rules_container = Frame(self.rule_frame)
             self.sub_rules_container.grid(row = 2, column = 1)
-            self.add_sub_rule_btn = Button(self.sub_rules_container, text = 'Add Sub-Rule', command = self.add_sub_rule)
-            self.add_sub_rule_btn.grid(row =0, column = 0)
-            self.sub_rules = []
-        elif self.selected_option.get() == 'Repeat Until':
-            # TODO
-            pass
+            self.add_if_sub_rule_btn = Button(self.sub_rules_container, text = 'Add If Sub-Rule', command = self.add_if_sub_rule)
+            self.add_if_sub_rule_btn.grid(row =0, column = 0, sticky = 'w', pady = (20,20))
+        elif self.selected_option.get() == 'Loop':
+            self.sub_rules_container = Frame(self.rule_frame)
+            self.sub_rules_container.grid(row = 2, column = 1, sticky = 'w')
+            self.add_loop_sub_rule_btn = Button(self.sub_rules_container, text = 'Add Loop Sub-Rule', command = self.add_loop_sub_rule)
+            self.add_loop_sub_rule_btn.grid(row = 0, column = 0, sticky ='w', pady = (20,20))
+        elif self.selected_option.get() == 'Exit When':
+            self.if_container = Frame(self.rule_frame)
+            self.if_container.grid(row = 0, column=1)
+            self.add_conditions_row()
 
-
-        elif self.selected_option.get() == 'Repeat Until':
-            pass
-    def add_sub_rule(self):
-        sub_rule = _RulesFrame(self.sub_rules_container, ['Set', 'Wait', 'Prompt', 'Repeat Until'])
-        sub_rule.rule_frame.grid(row = len(self.sub_rules)+1, column = 1, padx = (20,0))
-        self.add_sub_rule_btn.grid(row=len(self.sub_rules) + 2, column=1, pady = (0,25))
+    def add_if_sub_rule(self): # For if statement
+        sub_rule = _RulesFrame(self.sub_rules_container, ['Set', 'Wait', 'Prompt', 'Loop'], indent_level = self.indent_level + 1)
+        sub_rule.rule_frame.grid(row = len(self.sub_rules)+1, column = 0, sticky = 'w')
+        self.add_if_sub_rule_btn.grid(row=len(self.sub_rules) + 2, column=0, sticky = 'w')
         self.sub_rules.append(sub_rule)
+    def add_loop_sub_rule(self):
+        sub_rule = _RulesFrame(self.sub_rules_container, ['Set', 'Wait', 'Prompt', 'If', 'Exit When'], indent_level = self.indent_level + 1)
+        sub_rule.rule_frame.grid(row=len(self.sub_rules) + 1, column=1, sticky = 'w')
+        self.add_loop_sub_rule_btn.grid(row=len(self.sub_rules) + 2, column=0, sticky = 'w')
+        self.sub_rules.append(sub_rule)
+
 
     def add_conditions_row(self):
         row = _ConditionRow(self.if_container, self.on_logic_change)
